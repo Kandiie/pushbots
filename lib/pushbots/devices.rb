5153ad9afeeca3b5f9f64d9de4529a26e2414a47
Carlos Omana <araogaraog@gmail.com>
2016-03-21 13:25:59 -0600

    Add tags as optional in devices

--- lib/pushbots/devices.rb
+++ lib/pushbots/devices.rb
@@ -4,18 +4,15 @@ module Pushbots
     attr_accessor :tokens, :platform, :tags
     PLATFORM_TYPE = { ios: 0, android: 1 }.freeze
 
-    def initialize(tokens, platform, tags)
+    def initialize(tokens, platform, tags = nil)
       self.tokens = tokens
       self.platform = platform
-      self.tags = tags
+      self.tags = tags if tags
     end
 
     def register
-      body = {
-        tokens: tokens,
-        platform: PLATFORM_TYPE[platform],
-        tags: tags
-      }
+      body = { tokens: tokens, platform: PLATFORM_TYPE[platform] }
+      body[:tags] = tags if tags
       response = Request.register_batch(body)
       response.code == 201
     end
